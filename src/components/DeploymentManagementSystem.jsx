1663791b93c72f08fd715562ece5d6935e76b064
wjlander <will@w-j-lander.uk>
2025-09-20 05:09:59 +0100

    Updated DeploymentManagementSystem.jsx

--- src/components/DeploymentManagementSystem.jsx
+++ src/components/DeploymentManagementSystem.jsx
@@ -1046,4 +1046,4 @@ const DeploymentManagementSystem = ({ onLogout }) => {
                               <select
                                 defaultValue={position.area_id || ''}
                                 onChange={(e) => handleUpdatePosition(position.id, { area_id: e.target.value || null })}
-                                className="
+                                className="w-full border border-gray-300 rounded px-2 py-1 text-sm"
